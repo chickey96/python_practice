e6c1b1c0ed324db3d4214908d1bbb2c10267b31d
chickey96 <c.m.hickey06@gmail.com>
2020-08-12 15:51:58 -0700

    Create function that reverses words in a sentence

--- basic_algorithms.py
+++ basic_algorithms.py
@@ -1,13 +1,26 @@
 def sum_list(list):
   sum = 0
   for num in list:
-    # print(num)
     sum += num
 
-  # print(sum)
+  print(sum)
   return sum
 
-number_list = [1, 2, 3]
-print(sum_list(number_list))
-number_list.append(4)
-print(sum_list(number_list))
+# number_list = [1, 2, 3]
+# sum_list(number_list)
+# number_list.append(-4)
+# sum_list(number_list)
+
+# does not handle punctuation or capitalization at the moment
+def reverse_words(sentence):
+  words = sentence.split(" ")
+  answer = []
+
+  for word in words:
+    answer.append(word[::-1])
+
+  answer = " ".join(answer)
+  print(answer)
+  return answer
+
+reverse_words("Here are some words to reverse")
